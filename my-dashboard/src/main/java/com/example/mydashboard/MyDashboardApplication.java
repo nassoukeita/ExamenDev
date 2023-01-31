709fbe372050ade152b1b5aa0dbadbb8a103bdf1
nassoukeita <71669267+nassoukeita@users.noreply.github.com>
2023-01-31 16:27:11 +0100

    mise

--- my-dashboard/src/main/java/com/example/mydashboard/MyDashboardApplication.java
+++ my-dashboard/src/main/java/com/example/mydashboard/MyDashboardApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @EnableEurekaServer
 @EnableAdminServer
+
 public class MyDashboardApplication {
 
 	public static void main(String[] args) {
