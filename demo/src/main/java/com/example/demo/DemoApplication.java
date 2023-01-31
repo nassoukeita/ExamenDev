929556bdc14f96ae91ff21fc2e41617406667e3d
nassoukeita <71669267+nassoukeita@users.noreply.github.com>
2023-01-31 09:45:54 +0100

    fonct

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+    public {
+
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
